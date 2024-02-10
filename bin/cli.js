372941a6cc943aef1eec28e9b4b725c652521e2f
AlexandreLG2022 <a.legal.contact@gmail.com>
2024-02-10 05:33:08 +0100

    :zap: Created template bin

---
+++ bin/cli.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+
+const runCommand = (command) => {
+  try {
+    execSync(command, { stdio: "inherit" });
+  } catch (e) {
+    console.error(`Failed to execute command: ${command}:`, e);
+    return false;
+  }
+  return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/codebyalexx/project-template.git`;
+const installDepsCommand = `cd ${repoName} && pnpm install`;
+
+console.log(`Cloning repository in ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+const hasInstalledDeps = runCommand(installDepsCommand);
+if (!hasInstalledDeps) process.exit(-1);
+
+console.log(
+  "Congratulations! You are ready to start developing using this template. Run the following command to start:"
+);
+console.log(`cd ${repoName} && pnpm dev`);
